da49e6e303c37b6d2c90026fa4f4d9010b0282c7
Luke Eversfield <gesinn-it-evl@gesinn.it>
2023-07-04 11:57:05 +0200

    testing new action for generating tags

--- helpers.sh
+++ helpers.sh
@@ -11,6 +11,16 @@ function mediawiki_version() {
 		| tail -1
 }
 
+# function generate_tags { args : string baseImageName , string mediawikiFullVersion , string mediawikiVersion , string phpVersion , string phpDefault string variant } {
+# 	if [[ ${phpVersion} == ${phpDefault}  ]]; then
+# 		TAGS="${baseImageName}:${mediawikiFullVersion},${baseImageName}:${mediawikiVersion},${baseImageName}:${mediawikiFullVersion}-php${phpVersion},${baseImageName}:${mediawikiVersion}-php${phpVersion}"
+# 	else
+# 		TAGS="${baseImageName}:${mediawikiFullVersion}-php${phpVersion},${baseImageName}:${mediawikiVersion}-php${phpVersion}"
+# 	fi
+# 	echo "TAGS=$TAGS" >> $GITHUB_ENV
+# }
+
+
 declare -A variantExtras=(
 	[apache]='\n# Enable Short URLs\nRUN set -eux; \\\n\ta2enmod rewrite; \\\n\t{ \\\n\t\techo \"<Directory /var/www/html>\"; \\\n\t\techo \"  RewriteEngine On\"; \\\n\t\techo \"  RewriteCond %{REQUEST_FILENAME} !-f\"; \\\n\t\techo \"  RewriteCond %{REQUEST_FILENAME} !-d\"; \\\n\t\techo \"  RewriteRule ^ %{DOCUMENT_ROOT}/index.php [L]\"; \\\n\t\techo \"</Directory>\"; \\\n\t} > \"$APACHE_CONFDIR/conf-available/short-url.conf\"; \\\n\ta2enconf short-url\n\n# Enable AllowEncodedSlashes for VisualEditor\nRUN sed -i \"s/<\\/VirtualHost>/\\tAllowEncodedSlashes NoDecode\\n<\\/VirtualHost>/\" \"$APACHE_CONFDIR/sites-available/000-default.conf\"'
 	[fpm]=''
